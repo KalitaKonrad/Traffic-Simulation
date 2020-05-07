6df972f966df7d0736283ae7428a654c73349995
Kamil Wozniak <jestem.kamil.wozniak@gmail.com>
2020-05-07 22:09:15 +0200

    feat: Added basic Canvas setup.

--- src/Simulation.js
+++ src/Simulation.js
@@ -15,6 +15,17 @@ export default class Simulation {
         this.intersection.lights = 1;
         this.road.setIntersection(this.intersection);
         this.road.addVehicle(new Car(0,0,0,0));
+
+        this.canvas = document.querySelector('.js-canvas');
+        this.ctx = this.canvas.getContext('2d');
+
+        this.setUpCanvas();
+        window.addEventListener('resize', this.setUpCanvas.bind(this));
+    }
+
+    setUpCanvas() {
+        this.canvas.width = this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
     }
 
     update() {
@@ -23,12 +34,24 @@ export default class Simulation {
         console.log("Vehicles count ", this.road.vehicles.length);
     }
 
+    render() {
+        this.clear();
+        this.ctx.rect(20, 20, 150, 100);
+        this.ctx.stroke();
+      }
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
     loop() {
         this.update();
         this.update();
         this.road.addVehicle(new Car(0,0,0,0));
         for(let i=0; i< 20; i++){
             this.update();
+            this.clear();
+            this.render();
         }
     }
 }
